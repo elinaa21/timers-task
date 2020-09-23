79d9e05c0769d1b1c3eba52bacdcabb8550b500e
Elina <ella-abella@mail.ru>
2020-09-24 00:04:12 +0300

    layout and structure of blocks

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div  class="app">
-    <Timer />
+    <div class="container">
+      <Timer
+        v-for="timer of timers"
+        v-bind:timer="timer"
+        :key="timer.id"
+      />
+      <div class='add-timer'>&plus;</div>
+    </div>
   </div>
 </template>
 
@@ -8,7 +15,9 @@
 import Timer from './components/Timer.vue'
 
 const timers = [
-  { id: 0, hours: 0, minutes: 0, seconds: 0, active: false }
+  { id: 0, hours: 0, minutes: 0, seconds: 0, active: false },
+  { id: 1, hours: 0, minutes: 0, seconds: 0, active: false },
+  { id: 2, hours: 0, minutes: 0, seconds: 0, active: false },
 ];
 
 export default {
@@ -33,7 +42,29 @@ export default {
   width: 100%;
   height: 100%;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+}
+
+.container {
+  margin: 0 auto;
+  width: 55%;
+  height: max-content;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.add-timer {
+  width: 225px;
+  height: 120px;
+  background-color: #696969;
+  font-size: 25px;
+  text-align: center;
+  color: #9E9E9E;
+  margin-bottom: 7%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,19 +1,40 @@
 <template>
   <div class="timer">
-
+    <div 
+      class="timer__time"
+      v-bind:class="{timer__time_active: timer.active}"
+    >{{timer.hours}}:{{timer.minutes}}:{{timer.seconds}}</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Timer'
+  name: 'Timer',
+  props: {
+    timer: {
+      type: Object,
+      required: true
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style>
 .timer {
   width: 225px;
   height: 120px;
   background-color: #696969;
+  margin-bottom: 7%;
+}
+.timer__time {
+  font-size: 22px;
+  line-height: 21px;
+  color: #9E9E9E;
+  font-family: Gotham Pro;
+  height: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: #9E9E9E 1px solid;
 }
 </style>
