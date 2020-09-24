fdb5e0e244e6cfc3d8f8f89c75b8564c5d1f589c
Elina <ella-abella@mail.ru>
2020-09-24 21:22:55 +0300

    options for timers

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -13,10 +13,11 @@
         v-if="!timer.active" 
         @click="playTimer"
       ></div>
-      <div class="pause" v-else @click="timer.active = !timer.active"></div>
+      <div class="pause" v-else @click="pauseTimer"></div>
       <div 
         class="stop"
         v-bind:class="{stop_active: timer.active}"
+        @click="stopTimer"
       ></div>
     </div>
   </div>
@@ -33,7 +34,36 @@ export default {
   },
   methods: {
     playTimer() {
-      this.timer.active = !this.timer.active
+      this.timer.active = !this.timer.active;
+      this.intervalIDs[this.timer.id] = setInterval(() => {
+        if (this.timer.seconds === 59 && this.timer.minutes === 59) {
+          this.timer.hours++;
+          this.timer.minutes = 0;
+          this.timer.seconds = 0;
+        } else if (this.timer.seconds === 59) {
+          this.timer.minutes++;
+          this.timer.seconds = 0;
+        } else {
+          this.timer.seconds++;
+        }
+      }, 1000);
+    },
+    pauseTimer() {
+      this.timer.active = !this.timer.active;
+      clearInterval(this.intervalIDs[this.timer.id]);
+      delete this.intervalIDs[this.timer.id];
+    },
+    stopTimer() {
+      if (this.timer.active) {
+        this.timer.active = !this.timer.active;
+      }
+      if (this.intervalIDs[this.timer.id]) {
+        clearInterval(this.intervalIDs[this.timer.id]);
+        delete this.intervalIDs[this.timer.id];
+      }
+      this.timer.seconds = 0;
+      this.timer.minutes = 0;
+      this.timer.hours = 0;
     }
   },
   data() {
